0c9ac26cb9c0040e70ec01f079c1b7e593263fae
stephanie di dino <stephanie.didino@gmail.com>
2023-11-09 17:09:52 +0900

    Everything works except deleting a bookmark.

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :lists, only: [:index, :new, :create, :show]
   # get "lists", to: "lists#index"
 
   # get "lists/new", to: "lists#new"
   # post "lists", to: "lists#create"
 
   # get "lists/:id", to: "lists#show"
+
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  delete "bookmarks/:id", to: "bookmarks#destroy"
 end
