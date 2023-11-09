259de3e17294c0ee4df2f1a50e8bbd8bb9efb441
stephanie di dino <stephanie.didino@gmail.com>
2023-11-09 15:22:59 +0900

    Step 3 complete.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists, only: [:index, :new, :create, :show]
+  # get "lists", to: "lists#index"
+
+  # get "lists/new", to: "lists#new"
+  # post "lists", to: "lists#create"
+
+  # get "lists/:id", to: "lists#show"
 end
